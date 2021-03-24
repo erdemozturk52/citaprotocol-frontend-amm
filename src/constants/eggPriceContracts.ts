864d74f129356fdd320284967b7925d36dac4526
Erdem <44102290+erdemozturk52@users.noreply.github.com>
2021-03-24 13:35:14 +0300

    -

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,5 +1,5 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+  cakeAddress: '0xaFa2219bc6998575d2E5fd3EE602EAd4Ca9d7A65',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
   lpAddress: '0xC4E0876cb1547acb5a109Edf056aB888422295Ec'
 }
